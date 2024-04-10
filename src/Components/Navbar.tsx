6faaa16eafa0451d38f18a59a971dab2bfc8f109
prshanthreddy <varangantipr@gmail.com>
2024-04-09 21:46:41 -0500

    Navbar changes

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -5,7 +5,7 @@ import Search from "./search";
 export default function Navbar() {
   return (
     <div className="flex items-center justify-between bg-gray-900">
-      <h1 className="text-3xl font-bold text-white p-4">Blogpost</h1>
+      <h1 className="flex items-center text-white p-4 text-bolder hover:text-blue-500 justify-end">Blogpost</h1>
       <div className="flex items-center p-4">
         <Search />
       </div>
